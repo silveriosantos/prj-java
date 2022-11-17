f5788af351c976cb4f35f3f7264d55ffaf663c64
Silverio Dos Santos <silveriojannard@gmail.com>
2022-11-16 22:04:55 -0300

    Alterando nome das Strings

--- src/main/java/com/senai/prjjava/Entity/Usuario.java
+++ src/main/java/com/senai/prjjava/Entity/Usuario.java
@@ -12,30 +12,27 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.AUTO)
     
     private Integer id;
-    private String name;
+    private String nome;
     private String email;
-
+    
     public Integer getId() {
-        return id;
-      }
+      return id;
+    }
+    public void setId(Integer id) {
+      this.id = id;
+    }
+    public String getNome() {
+      return nome;
+    }
+    public void setNome(String nome) {
+      this.nome = nome;
+    }
+    public String getEmail() {
+      return email;
+    }
+    public void setEmail(String email) {
+      this.email = email;
+    }
+
     
-      public void setId(Integer id) {
-        this.id = id;
-      }
-    
-      public String getName() {
-        return name;
-      }
-    
-      public void setName(String name) {
-        this.name = name;
-      }
-    
-      public String getEmail() {
-        return email;
-      }
-    
-      public void setEmail(String email) {
-        this.email = email;
-      }
 }
